25483f9abb104e1db5fcdd0bdfdf4cb349075fe1
abdaloth <abdullahoalothman@gmail.com>
2021-05-29 03:37:07 -0400

    black reformatting

--- main_local.py
+++ main_local.py
@@ -2,23 +2,22 @@ import os
 import secrets
 from flask import Flask, render_template, request, flash, redirect, send_from_directory
 from werkzeug.utils import secure_filename
-
-from pydub import AudioSegment
 import torch
 import torchaudio
-
 from model import model, transform, channel, label_encoder
 
-UPLOAD_FOLDER = 'static/upload'
-ALLOWED_EXTENSIONS = {'mp3'}
-SAVED_NAME = 'tmp.mp3'
-TEN_SECONDS = 10046
+
+UPLOAD_FOLDER = "static/upload"
+ALLOWED_EXTENSIONS = {"mp3"}
+SAVED_NAME = "tmp.mp3"
+TEN_SECONDS_FRAMES = 443520
+
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
-
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 8 * 1024 * 1024
 app.secret_key = secrets.token_urlsafe(32)
+
 
 @app.route("/")
 def hello():
@@ -27,50 +26,56 @@ def hello():
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
+    if request.method == "POST":
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('ERROR: No file part')
+        if "file" not in request.files:
+            flash("ERROR: No file part")
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files["file"]
         # if user does not select file, browser also
         # submit an empty part without filename
-        if file.filename == '':
-            flash('ERROR: No selected file')
+        if file.filename == "":
+            flash("ERROR: No selected file")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            pth = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            pth = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             file.save(pth)
-            audio = AudioSegment.from_mp3(pth)
-            audio[:TEN_SECONDS].export(pth, format="mp3")
-            return render_template("index.html", audio_fname=filename, qari=predict(pth))
+            return render_template(
+                "index.html", audio_fname=filename, qari=predict(pth)
+            )
     return render_template("index.html")
 
-@app.route('/uploads/<filename>')
+
+@app.route("/uploads/<filename>")
 def uploaded_file(filename):
-    # audio = AudioSegment.from_mp3(src)
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+
+@app.errorhandler(413)
+def app_handle_413(e):
+    return render_template("index.html", qari="File is too Large")
 
 
 def predict(path):
     waveform, sample_rate = torchaudio.load(path)
-    sound_data = transform(waveform[channel, :])[0:13, :].unsqueeze(0)
+    if waveform.size()[1] < TEN_SECONDS_FRAMES:
+        return "Audio File is too short"
+
+    sound_data = transform(waveform[channel, :TEN_SECONDS_FRAMES])[0:13, :].unsqueeze(0)
 
     model.eval()
-    
+
     with torch.no_grad():
         output = model(sound_data)
 
     qari = label_encoder.inverse_transform([torch.argmax(output).item()])[0]
-        
+
     return qari
 
 
--- model.py
+++ model.py
@@ -19,76 +19,100 @@ num_classes = 77
 clip_length = 1109
 # learning_rate = 0.8
 dropout_rate = 0.2
-d_model = 13 # feature dimention
-dim_feedforward = 2048 # number of hidden
-num_layers = 2 # number of layers
-nhead = 13 # number of heads
-activation_function = 'relu'
+d_model = 13  # feature dimention
+dim_feedforward = 2048  # number of hidden
+num_layers = 2  # number of layers
+nhead = 13  # number of heads
+activation_function = "relu"
 clip = 5
 
 
 cpu_count = os.cpu_count()
 
 
-with open('static/label_encoder.pickle', 'rb') as input_file:
+with open("static/label_encoder.pickle", "rb") as input_file:
     label_encoder = pickle.load(input_file)
 
-sample_transform = torchaudio.transforms.Resample(orig_freq=original_sample_rate, new_freq=target_sample_rate)
-mfcc_transform = torchaudio.transforms.MFCC(sample_rate=target_sample_rate, n_mfcc=n_mfcc, log_mels=log_mels)
+sample_transform = torchaudio.transforms.Resample(
+    orig_freq=original_sample_rate, new_freq=target_sample_rate
+)
+mfcc_transform = torchaudio.transforms.MFCC(
+    sample_rate=target_sample_rate, n_mfcc=n_mfcc, log_mels=log_mels
+)
 
 transform = nn.Sequential(sample_transform, mfcc_transform)
 
 
 class Model(nn.Module):
-    def __init__(self, d_model, nhead, dim_feedforward, dropout_rate, activation_function, num_layers):
+    def __init__(
+        self,
+        d_model,
+        nhead,
+        dim_feedforward,
+        dropout_rate,
+        activation_function,
+        num_layers,
+    ):
         super(Model, self).__init__()
-        
-        encoder_layer = nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward=2048, dropout=dropout_rate, activation=activation_function)
-        
+
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model,
+            nhead,
+            dim_feedforward=2048,
+            dropout=dropout_rate,
+            activation=activation_function,
+        )
+
         normalization = nn.LayerNorm(d_model)
-                
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers, norm=normalization)
-        
+
+        self.transformer_encoder = nn.TransformerEncoder(
+            encoder_layer, num_layers, norm=normalization
+        )
+
         self.batch_normalization = nn.BatchNorm1d(num_features=clip_length)
 
         self.dropout = nn.Dropout(p=dropout_rate)
-        
-        self.linear = nn.Linear(clip_length*d_model, num_classes)
-        
-        
+
+        self.linear = nn.Linear(clip_length * d_model, num_classes)
+
     def forward(self, features):
 
-        
-        features = features.permute(2, 0, 1) # from (N,E,S) to (S,N,E)
-        
+        features = features.permute(2, 0, 1)  # from (N,E,S) to (S,N,E)
+
         encoder_output = self.transformer_encoder(features)
-        
-        encoder_output = encoder_output.permute(1, 0, 2) # from (S,N,E) to (N,S,E)
-        
+
+        encoder_output = encoder_output.permute(1, 0, 2)  # from (S,N,E) to (N,S,E)
+
         output_normalized = self.batch_normalization(encoder_output)
-        
+
         output_dropped = self.dropout(output_normalized)
-      
-        output_reshaped = output_dropped.reshape(-1, clip_length*d_model) # from (N,S,E) to (N,S*E)
-                        
+
+        output_reshaped = output_dropped.reshape(
+            -1, clip_length * d_model
+        )  # from (N,S,E) to (N,S*E)
+
         linear_output = self.linear(output_reshaped)
-                                                                
+
         return linear_output
 
 
-model = Model(d_model, nhead, dim_feedforward, dropout_rate, activation_function, num_layers)
-model.load_state_dict(torch.load('static/model_state_dict', map_location=torch.device('cpu')))
+model = Model(
+    d_model, nhead, dim_feedforward, dropout_rate, activation_function, num_layers
+)
+model.load_state_dict(
+    torch.load("static/model_state_dict", map_location=torch.device("cpu"))
+)
+
 
 def predict(path):
     waveform, sample_rate = torchaudio.load(path)
     sound_data = transform(waveform[channel, :])[0:13, :].unsqueeze(0)
 
     model.eval()
-    
+
     with torch.no_grad():
         output = model(sound_data)
 
     qari = label_encoder.inverse_transform([torch.argmax(output).item()])[0]
-        
-    return qari
 
+    return qari
